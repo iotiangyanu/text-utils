a136dfd7f7992740dd5607a996c19efdffd04129
iotangyanu <g.dwivedi8924@gmail.com>
2026-02-11 00:31:31 +0530

    Initial commit - Text Utils Django project

---
+++ textutils/views.py
@@ -0,0 +1,30 @@
+# i created this file -gyan
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+ 
+def uppercase(request):
+    text=request.GET.get('text','default')
+    upperchar=text.upper()
+    data={
+        'upper_case':upperchar
+    }
+    return render(request, 'uppercase.html', data)
+
+def charcount(request):
+    text=request.GET.get('text','default')
+    char='''`1234567890-=qwertyuiop[\]asdfghjkl;zxcvbnm,./~!@#$%^&*()_+}{|:"<>?QWERTYUIOPASDFGHJKLZXCVBNM'''
+    char=set(char)
+    data={
+        'count':0
+    }
+    for i in text:
+        if i in char:
+            data['count']+=1
+    return render(request, 'countchar.html', data)
+
+def about(request):
+    return render(request, 'about.html')
